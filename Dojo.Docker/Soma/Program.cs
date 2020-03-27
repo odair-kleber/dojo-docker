a0acd0a3ab954f306e2b0e310b9e4b0b706adc86
Odair Pedro <odairkleber@hotmail.com>
2020-03-27 18:25:27 -0300

    Setup projetos test

---
+++ Dojo.Docker/Soma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Soma
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var n1 = int.Parse(args[0]);
+                var n2 = int.Parse(args[1]);
+                
+                Console.WriteLine(n1 + n2);
+            }
+            catch
+            {
+                Console.WriteLine(Figgle.FiggleFonts.Standard.Render("ESTÁ FAZENDO ERRADO ANIMAL"));
+            }
+        }
+    }
+}
